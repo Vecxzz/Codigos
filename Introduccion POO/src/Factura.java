7bd5523407ad9d21ad85aa118add9bef4df5b317
Uriel Vera <112576496+Vecxzz@users.noreply.github.com>
2023-10-24 09:32:07 -0300

    Add files via upload

---
+++ Introduccion POO/src/Factura.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class Factura {
+    //Atributos
+    private int nrofactura;
+    private int fecha;
+    private String cliente;
+    private ArrayList<String> articulos;
+    private double preciounitario;
+    private int cantidad;
+    private double subtotal;
+    private double iva;
+    private double total;
+}
